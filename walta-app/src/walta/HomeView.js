7c5e3a2d951e4552f16f466f73c9b5150272652b
Michael Sharman <msharman@internode.on.net>
2013-04-17 23:13:31 +1000

    tweaked home view so that it is usable with a landscape orientation

--- walta-app/src/walta/HomeView.js
+++ walta-app/src/walta/HomeView.js
@@ -15,38 +15,38 @@ define( [ "dojo/_base/declare", "dojo/on", "dojo/dom-construct", "dojo/_base/lan
 			buildRendering: function() {
 				this.inherited(arguments);
 
-				domConstruct.create("div", { "class":"waltaLogo"}, this.domNode );
-				domConstruct.create("h1", { innerHTML: "WALTA"}, this.domNode );
-				domConstruct.create("h3", { innerHTML: "Waterbug ALT App"}, this.domNode );
+				var header = domConstruct.create("div", { "class":"waltaPanel waltaHeader"}, this.domNode );
 				
-				var menu = domConstruct.create("div", { "class":"waltaMenuPanel"}, this.domNode );
+				domConstruct.create("div", { "class":"waltaLogo"}, header );
+				domConstruct.create("h1", { innerHTML: "WALTA"}, header );
+				domConstruct.create("h3", { innerHTML: "Waterbug ALT App"}, header );
 				
-				var speedbug = domConstruct.create("div", { "class": "waltaPanel" }, menu);
+				var menu = this.domNode;
+				
+				var speedbug = domConstruct.create("div", { "class": "waltaPanel waltaMenu" }, menu);
 				domConstruct.create("div", { "class":"waltaMenuIcon waltaSpeedbugLogo"}, speedbug );
 				domConstruct.create("h2", { innerHTML: "Speedbug"}, speedbug );	
 				domConstruct.create("p", { innerHTML: "Look at silhouettes of bugs to choose the best match."}, speedbug );	
 				
-				var altkey = domConstruct.create("div", { "class": "waltaPanel" }, menu );
+				var altkey = domConstruct.create("div", { "class": "waltaPanel waltaMenu" }, menu );
 				domConstruct.create("div", { "class":"waltaMenuIcon waltaAltKeyLogo"}, altkey );
 				domConstruct.create("h2", { innerHTML: "ALT key"}, altkey );	
 				domConstruct.create("p", { innerHTML: "Questions to help identify your waterbug."}, altkey );
 				
-				var browse = domConstruct.create("div", { "class": "waltaPanel" }, menu );
+				var browse = domConstruct.create("div", { "class": "waltaPanel waltaMenu" }, menu );
 				domConstruct.create("div", { "class":"waltaMenuIcon waltaBrowseLogo"}, browse );
 				domConstruct.create("h2", { innerHTML: "Browse list"}, browse );	
 				domConstruct.create("p", { innerHTML: "If you know the name or scientific name of your bug."}, browse );
 				
-				var menu2 = domConstruct.create("div", { "class":"waltaMenuPanelSecondary"}, menu );
-				
-				var help = domConstruct.create("div", { "class": "waltaPanel" }, menu2);
+				var help = domConstruct.create("div", { "class": "waltaPanel waltaMenu waltaSecondary" }, menu);
 				domConstruct.create("h2", { innerHTML: "Help"}, help );	
 				domConstruct.create("p", { innerHTML: "Info to get you started."}, help );
 				
-				var gallery = domConstruct.create("div", { "class": "waltaPanel" }, menu2 );
+				var gallery = domConstruct.create("div", { "class": "waltaPanel waltaMenu waltaSecondary" }, menu );
 				domConstruct.create("h2", { innerHTML: "Gallery"}, gallery );	
 				domConstruct.create("p", { innerHTML: "Browse photos & videos."}, gallery );	
 				
-				var about = domConstruct.create("div", { "class": "waltaPanel" }, menu2 );
+				var about = domConstruct.create("div", { "class": "waltaPanel waltaMenu waltaSecondary" }, menu );
 				domConstruct.create("h2", { innerHTML: "About"}, about );	
 				domConstruct.create("p", { innerHTML: "About the app."}, about );	
 
