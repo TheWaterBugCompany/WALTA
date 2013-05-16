66cf494954479bc73d4d13cddd8398fbac4e3aad
Michael Sharman <msharman@internode.on.net>
2013-05-16 10:44:24 +1000

    Updated test build

--- walta-build/platform/android/assets/www/walta/walta.js.uncompressed.js
+++ walta-build/platform/android/assets/www/walta/walta.js.uncompressed.js
@@ -1731,6 +1731,27 @@ define("dojox/mobile/lazyLoadUtils", [
 
 
 },
+'walta/SpeedBugTile':function(){
+/*
+ * walta/SpeedBugTile
+ *
+ * Creates a tile for the SpeedBug widget
+ *  
+ */
+define( "walta/SpeedBugTile", [ "dojo/_base/declare", "dojo/_base/lang", "dojo/dom-construct", "dojo/on", "dojo/topic", "dojox/mobile/Pane", "dijit/_Contained" ], 
+	function( declare, lang, domConstruct, on, topic, Pane, Contained ) {
+		return declare( "walta.SpeedBugTile", [Pane, Contained], {
+			image: "",
+			ref: "",
+			baseClass: "waltaSpeedBugTile",
+			buildRendering: function() {
+				this.inherited(arguments);
+				var tile = domConstruct.create("img", { "class":"waltaSpeedBugImage", src: this.image }, this.containerNode );
+				on( tile, "click", lang.hitch( this, function(e) { topic.publish("key/jump", this.ref ); } ) );
+			}
+		});
+});
+},
 'dijit/_WidgetBase':function(){
 define("dijit/_WidgetBase", [
 	"require",			// require.toUrl
