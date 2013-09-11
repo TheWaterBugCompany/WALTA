961b9ee25430e2d9396eb96bec13ce1e6d2bc88a
Michael Sharman <msharman@internode.on.net>
2013-09-11 15:23:54 +1000

    removed old test harness

--- walta-app-new/Resources/ui-test/KeyLoaderTest.js
+++
@@ -1,7 +0,0 @@
-var Key = require('logic/Key');
-var KeyLoaderXml = require('logic/KeyLoaderXml');
-
-function run() {
-	key = KeyLoaderXml.loadKey( 'ui-test/resources/simpleKey1' );
-}
-exports.run = run;
