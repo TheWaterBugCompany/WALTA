9b078a3a982dd8869a1a05cfa4512381b963dd9d
Michael Sharman <michael@thecodesharman.com.au>
2018-04-02 20:24:37 +1000

    install chai and mocha via npm install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+SPECS_LIB_DIR=walta-app/app/specs/lib
+if [ ! -d $SPECS_LIB_DIR ]; then
+  mkdir $SPECS_LIB_DIR
+fi
+cp ./node_modules/chai/chai.js $SPECS_LIB_DIR/chai.js
+cp ./node_modules/mocha/mocha.js $SPECS_LIB_DIR/mocha.js
