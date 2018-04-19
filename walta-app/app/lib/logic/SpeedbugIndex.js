98a09d292f5de909780dfa1c24d30595ecf46ef9
Michael Sharman <michael@thecodesharman.com.au>
2018-04-19 19:42:52 +1000

    added reverse lookup from taxon to speedbug

--- walta-app/app/lib/logic/SpeedbugIndex.js
+++ walta-app/app/lib/logic/SpeedbugIndex.js
@@ -4,6 +4,7 @@ function createSpeedbugIndex() {
   var speedBugIndex = {};
   return {
     speedBugIndex: speedBugIndex,
+
     // Adds a "Speed Bug" link, this allows a special index
     // to be displayed that jumps directly to a node within
     // a key by touching a silhouette image.
@@ -18,6 +19,32 @@ function createSpeedbugIndex() {
 
     addSpeedbugIndex: function( imgUrl, grpId, refId) {
       speedBugIndex[grpId].bugs.push( { imgUrl: imgUrl, refId: refId } );
+    },
+
+    // Searches for the speedbug silhouette for this taxon
+		findSilhouette: function( node ) {
+      if ( node.id ) {
+        var bug = this.reverseLookup( node.id );
+        console.log(`found bug: ${bug}`);
+        if ( bug )
+          return bug.imgUrl;
+      }
+
+      if ( node.parentLink )
+        return this.findSilhouette( node.parentLink );
+		},
+
+    reverseLookup: function( refId ) {
+      console.log(refId);
+      var foundBug;
+      _(this.speedBugIndex).each(
+        (grp) => grp.bugs.forEach( (bug) => {
+          if ( bug.refId == refId ) {
+            foundBug = bug;
+          }
+        })
+      );
+      return foundBug;
     },
 
     getSpeedbugIndex: function() {
--- walta-app/app/specs/Taxon_spec.js
+++ walta-app/app/specs/Taxon_spec.js
@@ -15,19 +15,23 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-require("specs/lib/ti-mocha");
+require("specs/lib/mocha");
 var { expect } = require('specs/lib/chai');
 var Taxon = require('logic/Taxon');
-describe.skip('Taxon tests', function() {
+describe('Taxon tests', function() {
 	var txn = Taxon.createTaxon({
 		id: "testTaxon",
 		name: "Family Palaemonidae, Genus Macrobrachium",
+		scientificName: [
+			{ taxonomicLevel: 'Family', name: 'Palaemonidae' },
+			{ taxonomicLevel: 'Genus', name: 'Macrobrachium' } ],
 		commonName: "Freshwater prawn",
 		size: 300,
 		habitat: "Crayfish in rivers (upper photo) yabbies in wetlands/pools (lower photo).",
 		movement: "walking, with sudden flips when disturbed.",
 		confusedWith: "Nothing, very distinctive, We have left crayfish and Yabbies grouped together because they mostly turn up as juveniles in samples and are difficult to spearate when young.",
 		signalScore: 4,
+		description: "Some Notes",
 		mediaUrls: [ "resources/simpleKey1/media/amphipoda_01.jpg", "resources/simpleKey1/media/amphipoda_02.jpg", "resources/simpleKey1/media/attack_caddis_01_x264.mp4" ],
 		parentLink: {
 				id: "textTaxon2",
@@ -42,28 +46,27 @@ describe.skip('Taxon tests', function() {
 		}
 	);
 	it('should store taxon properties', function(){
-		expect(txn.id).toEqual("testTaxon");
-		expect(txn.name).toEqual("Family Palaemonidae, Genus Macrobrachium");
-		expect(txn.commonName).toEqual("Freshwater prawn");
-		expect(txn.size).toEqual(300);
-		expect(txn.habitat).toEqual("Crayfish in rivers (upper photo) yabbies in wetlands/pools (lower photo).");
-		expect(txn.movement).toEqual("walking, with sudden flips when disturbed.");
-		expect(txn.confusedWith).toEqual("Nothing, very distinctive, We have left crayfish and Yabbies grouped together because they mostly turn up as juveniles in samples and are difficult to spearate when young.");
-		expect(txn.signalScore).toEqual(4);
-		expect(txn.mediaUrls).toEqual([ "resources/simpleKey1/media/amphipoda_01.jpg", "resources/simpleKey1/media/amphipoda_02.jpg", "resources/simpleKey1/media/attack_caddis_01_x264.mp4" ]);
+		expect(txn.id).to.equal("testTaxon");
+		expect(txn.name).to.equal("Family Palaemonidae, Genus Macrobrachium");
+		expect(txn.commonName).to.equal("Freshwater prawn");
+		expect(txn.size).to.equal(300);
+		expect(txn.habitat).to.equal("Crayfish in rivers (upper photo) yabbies in wetlands/pools (lower photo).");
+		expect(txn.movement).to.equal("walking, with sudden flips when disturbed.");
+		expect(txn.confusedWith).to.equal("Nothing, very distinctive, We have left crayfish and Yabbies grouped together because they mostly turn up as juveniles in samples and are difficult to spearate when young.");
+		expect(txn.signalScore).to.equal(4);
+		expect(txn.mediaUrls).to.have.members([ "resources/simpleKey1/media/amphipoda_01.jpg", "resources/simpleKey1/media/amphipoda_02.jpg", "resources/simpleKey1/media/attack_caddis_01_x264.mp4" ]);
 	});
 	it('should determine the media type from the extension names', function(){
-		expect(txn.photoUrls).toEqual([ "resources/simpleKey1/media/amphipoda_01.jpg", "resources/simpleKey1/media/amphipoda_02.jpg" ] );
-		expect(txn.videoUrl).toEqual( "resources/simpleKey1/media/attack_caddis_01_x264.mp4" );
+		expect(txn.photoUrls).to.have.members([ "resources/simpleKey1/media/amphipoda_01.jpg", "resources/simpleKey1/media/amphipoda_02.jpg" ] );
+		expect(txn.videoUrl).to.equal( "resources/simpleKey1/media/attack_caddis_01_x264.mp4" );
 	});
 	it('should return the detail as HTML properly', function(){
-		expect(txn.asDetailHtml()).toEqual("<b>Family Palaemonidae, Genus Macrobrachium</b><p><b>Size:</b> Up to 300mm</p>"
-			+   "<p><b>Habitat:</b> Crayfish in rivers (upper photo) yabbies in wetlands/pools (lower photo).</p>"
-			+   "<p><b>Movement:</b> walking, with sudden flips when disturbed.</p>"
+		expect(txn.asDetailHtml()).to.equal("<p><b>Scientific Classification:</b><br>Family: Palaemonidae<br>Genus: Macrobrachium<br></p><p><b>Size:</b> Up to 300mm</p>"
+			+ "<p><b>Habitat:</b> Crayfish in rivers (upper photo) yabbies in wetlands/pools (lower photo).</p>"
+			+ "<p><b>Movement:</b> walking, with sudden flips when disturbed.</p>"
 			+	"<p><b>Confused with:</b> Nothing, very distinctive, We have left crayfish and Yabbies grouped together because they mostly turn up as juveniles in samples and are difficult to spearate when young.</p>"
 			+	"<p><b>SIGNAL score: 4</b></p>"
-			+   "<p>parent name<br>parent parent name</p>"
-			+   "<p></p>");
+			+ "<p>Some Notes</p>");
 	});
 
 });
