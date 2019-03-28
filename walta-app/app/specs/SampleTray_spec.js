2c477b0d94c85c60870aca41cba0cbca747325e3
Michael Sharman <michael@thecodesharman.com.au>
2019-03-28 21:07:52 +1100

    fixed summary screen layout for large screens

--- walta-app/app/specs/SampleTray_spec.js
+++ walta-app/app/specs/SampleTray_spec.js
@@ -343,7 +343,7 @@ describe( 'SampleTray controller', function() {
       the implementation - we need to look up expect tile positions by
       there coordinates in the view. */
 
-    it.only('when scrolled to the right it should update the screen properly', function() {
+    it('when scrolled to the right it should update the screen properly', function() {
       return Promise.resolve()
           .then( openSampleTray )
           .then( scrollSampleTray( 209*4 ) )
