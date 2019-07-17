583bbf1acb36a9689e192dd16bec92b1b7344d10
Michael Sharman <michael@thecodesharman.com.au>
2019-07-18 01:37:12 +1000

    remove reference to appcelerate cli and rely on base titanium cli - also do not use global packages. This is avoid annoying conflicts which cause a broken sql.js adapter to break builds.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,26 +6,40 @@ module.exports = function(grunt) {
     const KEYSTORE_SUBKEY = process.env.KEYSTORE_SUBKEY || 'thecodesharman';
     
     // Project configuration.
+    function build(extra_args) {
+      return {
+        command: `./node_modules/.bin/titanium build --project-dir walta-app ${extra_args}`, 
+        stdout: 'inherit', 
+        stderr: 'inherit',
+        options: { 
+          env: Object.assign({}, process.env, {
+            "ALLOY_PATH": "./node_modules/.bin/alloy"
+          })
+        } 
+      }
+    }
+
     grunt.initConfig({
       exec: {
           mock_server: {
             command: 'node mock-server', stdout: 'inherit', stderr: 'inherit'
           },
           alloy_plugins: {
-            command: `alloy install plugin walta-app`
+            command: `./node_modules/.bin/alloy install plugin walta-app`
           },
           clean: {
-            command: `appc ti clean --project-dir walta-app`, stdout: 'inherit', stderr: 'inherit'
+            command: './node_modules/.bin/titanium clean --project-dir ./walta-app', stdout: 'inherit', stderr: 'inherit'
           },
 
           unit_test_android: {
             command: `adb shell am start -n "net.thewaterbug.waterbug/.WaterbugActivity --ez "android.intent.action.UnitTest" true`
           },
 
-          build: {
-            command: `appc ti build --build-only --project-dir walta-app --target emulator --platform android --deploy-type development`, stdout: 'inherit', stderr: 'inherit'
-          },
-          uninstall_app: `adb uninstall ${APP_ID}`,
+          build: build("--build-only --target emulator --platform android --deploy-type development"),
+
+          install_app_android: `adb install ./walta-app/build/android/bin/Waterbug.apk`,
+
+          uninstall_app_android: `adb uninstall ${APP_ID}`,
           // test: "calabash-android run walta-app/build/android/bin/Waterbug.apk features/registration.feature --tags @only"
           acceptance_test: {
             command: `./node_modules/.bin/cucumber-js --tags @only`, stdout: 'inherit', stderr: 'inherit'
@@ -33,14 +47,14 @@ module.exports = function(grunt) {
           // test_console: "calabash-android console walta-app/build/android/bin/Waterbug.apk features/submit_sample.feature"
          // unit_test: `appc ti build --project-dir walta-app --target emulator --device-id ${AVD_NAME} --liveview --platform android --deploy-type development`,
           unit_test_node: `NODE_PATH="./walta-app/app:./walta-app/app/lib" mocha --compilers js:babel-core/register walta-app/app/specs/CerdiApi_spec.js`,
-          clean: `rm -rf walta-app/build/* && rm -rf walta-app/dist/* && rm -rf walta-app/Resources/*`,
-          debug: `appc ti build --project-dir walta-app --platform android --target emulator --device-id ${AVD_NAME} --debug-host /127.0.0.1:38331`,
-          preview_android: `appc ti build  --project-dir walta-app --platform android --deploy-type development --liveview --target emulator --device-id ${AVD_NAME}`,
-          preview_ios: `appc ti build --project-dir walta-app --platform ios --deploy-type development --target simulator --liveview --device-id "0797B956-B9F1-4AF5-BFC8-085A97F7F34B"`,
-          device_preview_android: `appc ti build --force --project-dir walta-app --platform android --deploy-type development --target device`,
-          device_preview_ios: `appc ti build --project-dir walta-app --platform ios -V  \"Michael Sharman (ZG6HRCUR8Q)\"  -P \"9bc28620-8680-4eea-9458-c346b32fb4f2\" --deploy-type development --target device `,
-          release_ios: `appc ti build --project-dir walta-app --build-only --platform ios -R  \"Michael Sharman (6RRED3LUUV)\" -P \"e2935a1f-0c22-4716-8020-b61024ce143f\" --target dist-appstore --output-dir release`,
-          release_android: `appc ti build --project-dir walta-app --build-only --platform android  --target dist-playstore --keystore ${KEYSTORE} --store-password ${KEYSTORE_PASSWORD} --alias ${KEYSTORE_SUBKEY} --output-dir release`
+          //clean: `rm -rf walta-app/build/* && rm -rf walta-app/dist/* && rm -rf walta-app/Resources/*`,
+          debug: build(`--platform android --target emulator --device-id ${AVD_NAME} --debug-host /127.0.0.1:38331`),
+          preview_android: build(`--platform android --deploy-type development --target emulator --device-id ${AVD_NAME}`),
+          preview_ios: build(`--platform ios --deploy-type development --target simulator --liveview --device-id "0797B956-B9F1-4AF5-BFC8-085A97F7F34B"`),
+          device_preview_android: build(`--platform android --deploy-type development --target device`),
+          device_preview_ios: build(`--platform ios -V  \"Michael Sharman (ZG6HRCUR8Q)\"  -P \"9bc28620-8680-4eea-9458-c346b32fb4f2\" --deploy-type development --target device `),
+          release_ios: build(`--build-only --platform ios -R  \"Michael Sharman (6RRED3LUUV)\" -P \"e2935a1f-0c22-4716-8020-b61024ce143f\" --target dist-appstore --output-dir release`),
+          release_android: build(` --build-only --platform android  --target dist-playstore --keystore ${KEYSTORE} --store-password ${KEYSTORE_PASSWORD} --alias ${KEYSTORE_SUBKEY} --output-dir release`)
         },
         newer: {
           titanium_build: {
@@ -48,7 +62,7 @@ module.exports = function(grunt) {
                       './walta-app/app/**/*.xml', 
                       './walta-app/app/**/*.css' ],
             dest: './walta-app/build/android/bin/Waterbug.apk',
-            options: { tasks: [ 'exec:clean', 'exec:build' ] }  
+            options: { tasks: [ 'exec:clean', 'exec:uninstall_app_android', 'exec:build', 'exec:install_app_android' ] }  
           }
         }
     });
@@ -60,7 +74,7 @@ module.exports = function(grunt) {
   
     // Default task(s).
     grunt.registerTask('default', ['build'] );
-    grunt.registerTask('build', ['exec:alloy_plugins', 'exec:build'] );
+    grunt.registerTask('build', ['exec:clean', 'exec:alloy_plugins', 'exec:build'] );
     grunt.registerTask('acceptance_test', [ 'newer:titanium_build', 'exec:acceptance_test']);
     grunt.registerTask('unit_test_android', [ 'exec:unit_test_android' ] );
     grunt.registerTask('unit_test_node', ['exec:unit_test_node'] );
