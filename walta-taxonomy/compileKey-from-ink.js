f2f1c41efbb9580fed5addf54414b786ff0e4a02
Michael Sharman <michael@thecodesharman.com.au>
2019-09-14 21:47:44 +1000

    verification of key

--- walta-taxonomy/compileKey-from-ink.js
+++ walta-taxonomy/compileKey-from-ink.js
@@ -1,6 +1,7 @@
-#! /usr/bin/node
+#! /usr/local/bin/node
 var fs = require('fs');
 var CircularJSON = require('circular-json');
 var KeyLoader = require("logic/KeyLoaderInk");
 var key = KeyLoader.loadKey( '../walta-app/app/assets/taxonomy/walta/', '/taxonomy/walta' );
+console.log( `writing file walta/key.json`);
 fs.writeFileSync( 'walta/key.json', CircularJSON.stringify(key) );
