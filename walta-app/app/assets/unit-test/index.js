2107e737bd52ca668d37f7652a22e73583a3e683
Michael Sharman <michael@thecodesharman.com.au>
2019-09-18 23:40:43 +1000

    added infinite loop mode to unit tests

--- walta-app/app/assets/unit-test/index.js
+++ walta-app/app/assets/unit-test/index.js
@@ -1,42 +1,64 @@
-require("unit-test/lib/ti-mocha");
+var Mocha = require("unit-test/lib/ti-mocha"); 
 
-[ "AnchorBar", 
-  "TaxonList",
-  "Habitat",
-  "KeyNode",
-  "KeySearch",
-  "LogIn",
-  "MediaUtil",
-  "MethodSelect",
-  "Menu",
-  "Register",
-  "QuestionController",
-  "SampleTray",
-  "Sample",
-  "Speedbug",
-  "Summary",
-  "TaxonDetails",
-  "SiteDetails",
-  "VideoView",
-  "ViewUtils",
-  "LocationEntry",
-  "LeafletMap",
-  "MayflyEmergenceMap",
-  "MayflyMusterSelect",
-  "Gallery",
-  "PhotoSelect",
-  "EditTaxon",
-].forEach( (f) =>  {
-  let specPath = `unit-test/${f}_spec`;
-  require( specPath );
-});
+function runTests() {
+  let mocha = new Mocha({
+    ui: 'bdd',
+    reporter: 'ti-spec'
+  });
+  return new Promise( function(resolve, reject) {
+    [ "AnchorBar", 
+      "TaxonList",
+      "Habitat",
+      "KeyNode",
+      "KeySearch",
+      "LogIn",
+      "MediaUtil",
+      "MethodSelect",
+      "Menu",
+      "Register",
+      "QuestionController",
+      "SampleTray",
+      "Sample",
+      "Speedbug",
+      "Summary",
+      "TaxonDetails",
+      "SiteDetails",
+      "VideoView",
+      "ViewUtils",
+      "LocationEntry",
+      "LeafletMap",
+      "MayflyEmergenceMap",
+      "MayflyMusterSelect",
+      "Gallery",
+      "PhotoSelect",
+      "EditTaxon",
+    ].forEach( (f) => {
+      let specPath = `unit-test/${f}_spec`;
+      if ( __remove_module_from_preview_cache ) {
+        __remove_module_from_preview_cache(specPath);
+      }
+      mocha.addFile(specPath);
+    });
+    return mocha.run(resolve);
+  });
+}
 
-
+// useful for testing memory leaks
+infinteLoopMode = (ENV === 'liveview');
 
 // Create a blank window: for some reason closing the last window hangs 
 // the test suite.
 var backgroundWindow = Ti.UI.createWindow( { backgroundColor: "black" } );
 backgroundWindow.addEventListener('open' , function() {
-  mocha.run();
+    let i = 0;
+   function forever(first, fn) {
+      console.log(`\n\n${++i} ===========================================\n`)
+      return first.then(fn).then( () => forever(Promise.resolve(),fn)); 
+   }
+   // run forever to allow memory leak detection
+   if ( infinteLoopMode ) 
+      forever( Promise.resolve(), runTests );
+   else
+      runTests();
 } );
 backgroundWindow.open();
