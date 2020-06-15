317c7b1d72c6b7070cc74762bb4f3c540919ac2a
Michael Sharman <michael@thecodesharman.com.au>
2020-06-15 22:39:04 +1000

    update key to include new adult mayfly nodes and add grunt task for building key
    Former-commit-id: bef4775c4411cffe35e8cd353679d43159439373

--- walta-taxonomy/compileKey-from-ink.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/node
-var fs = require('fs');
-var CircularJSON = require('circular-json');
-var KeyLoader = require("logic/KeyLoaderInk");
-var key = KeyLoader.loadKey( '../walta-app/app/assets/taxonomy/walta/', '/taxonomy/walta' );
-console.log( `writing file walta/key.json`);
-fs.writeFileSync( 'walta/key.json', CircularJSON.stringify(key) );
