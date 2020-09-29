8c50954c45ae3b6da2a57c73b4fd2dd80ddd544d
Michael Sharman <michael@thecodesharman.com.au>
2020-09-29 12:08:13 +1000

    added retrieveSamples()
    Former-commit-id: 4334caeffd4c850036925a44c7e284b4c7ccb5fb

--- walta-app/app/lib/util/Crashlytics.js
+++ walta-app/app/lib/util/Crashlytics.js
@@ -1,8 +1,8 @@
 
-// Interface for Crashlytics module - alos loaded during Node based unit tests
+// Interface for Crashlytics module - also loaded during Node based unit tests
 exports.configure = function() {}
 exports.isAvailable = function() {}
 exports.setCustomKey = function(name, value) {}
-exports.recordException = function(e) {}
+exports.recordException = function(e) { console.error(e) }
 exports.setUserId = function(userId) {}
 exports.log = function( message ) {}
