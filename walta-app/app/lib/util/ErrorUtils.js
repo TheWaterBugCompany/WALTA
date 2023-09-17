db34cecca83ba2fe08991d9f174468c16413bf75
Michael Sharman <michael@thecodesharman.com.au>
2023-09-17 20:09:38 +1000

    Fix Error Reporting
    Former-commit-id: e62cf3d0bf7d5d1e3a46baf3afb883f6799f6ebf

--- walta-app/app/lib/util/ErrorUtils.js
+++ walta-app/app/lib/util/ErrorUtils.js
@@ -3,26 +3,26 @@ var log = Logger.log;
 var debug = m => Ti.API.info(m);
 function formatError(err) {
     let message = "<unknown error>";
+
     if ( err.error ) {
         message = err.error;
+    } else if ( err.errors ) {
+        message = `${err.message} ${JSON.stringify(err.errors)}`;
     } else if ( err.message ) {
-        message = err.message;
+        message = err.message
     }
+
     if ( err.source ) {
-        message = `${message} Details: ${JSON.stringify(err.source)}` 
+        message = `${message} Details: ${JSON.stringify(err.source)}`;
     }
     if ( err.stack ) {
-        message = `${message} ${err.stack}` 
+        message = `${message} ${err.stack}`;
     }
     return message;
 }
 function errorHandler( err ) {
-    if ( err.message === "The given data was invalid.") {
-        var errors = _(err.errors).values().map((e)=> e.join("\n")).join("\n");
-        log(`Data was invalid continuing: ${errors}`);
-    } 
     Logger.recordException( err );
-    return Promise.reject(err);
+    //return Promise.reject(err);
 }
 exports.errorHandler = errorHandler;
 exports.formatError = formatError;
